8358414d85204137d4857035bf92c8c9b193f6f3
Mr Bones <62398642+TheRealMrBones@users.noreply.github.com>
2025-01-07 08:51:21 -0500

    minor changes to form dependencies

--- public/js/form.js
+++ public/js/form.js
@@ -1,11 +1,19 @@
+const form = document.querySelector('form');
+
+form.addEventListener('keypress', function (event) {
+    if (event.key.toLowerCase() == "enter" && event.target.tagName.toLowerCase() == "input" && event.target.type.toLowerCase() == "text") {
+        event.preventDefault();
+        form.submit();
+    }
+});
+
 function showError(message) {
     // Display error using Bootstrap alert
     var alertDiv = document.createElement("div");
-    alertDiv.className = "alert alert-danger mt-3";
+    alertDiv.className = "erroralert";
     alertDiv.textContent = message;
 
     // Insert the alert above the form
-    var form = document.querySelector("form");
     form.parentNode.insertBefore(alertDiv, form.nextSibling);
 
     // Remove the alert after a few seconds
@@ -17,11 +25,10 @@ function showError(message) {
 function showSuccess(message) {
     // Display error using Bootstrap alert
     var alertDiv = document.createElement("div");
-    alertDiv.className = "alert alert-success mt-3";
+    alertDiv.className = "successalert";
     alertDiv.textContent = message;
 
     // Insert the alert above the form
-    var form = document.querySelector("form");
     form.parentNode.insertBefore(alertDiv, form.nextSibling);
 
     // Remove the alert after a few seconds
